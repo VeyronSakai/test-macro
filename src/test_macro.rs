92fa72eff3e5b223779d0dfd91f055dd733cb46c
Yuki Sakai <yuukisakai123@gmail.com>
2022-02-13 17:05:04 +0900

    write doc comment of test_eq

--- src/test_macro.rs
+++ src/test_macro.rs
@@ -1,3 +1,22 @@
+/// Generate a test code that internally uses assert_eq!.
+///
+/// The first argument should be the test case name,
+/// and the second argument should be the arguments and the expected return value.
+/// (Put the arguments to the left of => and the return value to the right)
+///
+/// # Example
+///
+/// ```
+/// fn add(x: i32, y: i32) -> i32 {
+///      x + y
+/// }
+/// ```
+///
+/// If you want to test this add function, you can write it as follows
+///
+/// ```
+/// test_macro::test_eq!(test_case_name, add(1, 2) => 3);
+/// ```
 #[macro_export]
 macro_rules! test_eq {
     ($func_name:ident, $arg:expr => $ans:expr) => {
